7de3d9878633e568b52b0c8982e4dc77f7297251
Harsha Narayana <etcharsha04@gmail.com>
2015-08-02 16:12:18 -0500

    Changes to RubberDuck.java and WoodenDuck.java

--- strategy_pattern/src/com/harsha/strategy/RubberDuck.java
+++ strategy_pattern/src/com/harsha/strategy/RubberDuck.java
@@ -2,6 +2,10 @@ package com.harsha.strategy;
 
 public class RubberDuck extends Duck {
 
+	public void quack(){
+		System.out.println("I squeak instead of quack...");
+	}
+	
 	public void display(){
 		System.out.println("I am a rubber duck");
 	}
--- strategy_pattern/src/com/harsha/strategy/WoodenDuck.java
+++ strategy_pattern/src/com/harsha/strategy/WoodenDuck.java
@@ -2,6 +2,10 @@ package com.harsha.strategy;
 
 public class WoodenDuck extends Duck {
 
+	public void quack(){
+		System.out.println("I can't quack...");
+	}
+	
 	public void display(){
 		System.out.println("I am a WoodenDuck...");
 	}
