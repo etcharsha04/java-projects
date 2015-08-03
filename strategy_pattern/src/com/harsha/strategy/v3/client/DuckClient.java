a28f6523872d7d505317f4037f5800af069bef47
Harsha Narayana <etcharsha04@gmail.com>
2015-08-03 00:52:50 -0500

    Changes to Duckclient.java

--- strategy_pattern/src/com/harsha/strategy/v3/client/DuckClient.java
+++ strategy_pattern/src/com/harsha/strategy/v3/client/DuckClient.java
@@ -5,7 +5,9 @@ import com.harsha.strategy.v3.MallardDuck;
 import com.harsha.strategy.v3.RedheadDuck;
 import com.harsha.strategy.v3.RubberDuck;
 import com.harsha.strategy.v3.WoodenDuck;
+import com.harsha.strategy.v3.behaviors.FlyNoWay;
 import com.harsha.strategy.v3.behaviors.FlyWithWings;
+import com.harsha.strategy.v3.behaviors.Silent;
 
 public class DuckClient {
 
@@ -16,6 +18,20 @@ public class DuckClient {
 		duck.display();
 		duck.swim();	
 		duck.performFly();
+		duck.performQuack();
+		
+		System.out.println("------------------------------------");
+		
+		// Dynamically changing the behavior of MallardDuck.
+		duck.display();
+		duck.swim();
+		
+		// Dynamically changing the fly behavior of MallardDuck.
+		duck.setFlyBehavior(new FlyNoWay());
+		duck.performFly();
+		
+		//Dynamically changing the quack behavior of MallardDuck.
+		duck.setQuackBeharior(new Silent());
 		duck.performQuack();
 		
 		System.out.println("------------------------------------");
